16847393b02df098a0ccdd1ceafa5556c0284be1
Igit Purwahyudi <igithks@gmail.com>
2022-10-22 22:24:54 +0700

    add avatar

--- src/router/v1/defaultRouter.js
+++ src/router/v1/defaultRouter.js
@@ -5,6 +5,8 @@ router.route('/check-api').get(defaultController.testAPI);
 router.route('/get-all-users').get(defaultController.getAllUsers);
 router.route('/get-user').get(defaultController.getUser);
 router.route('/get-comment').get(defaultController.getComment);
+router.route('/get-all-avatar').get(defaultController.getAllAvatar);
+router.route('/get-avatar').get(defaultController.getAvatarByName);
 
 router.route('/create-user').post(defaultController.createUser);
 router.route('/post-comment').post(defaultController.postComment);
